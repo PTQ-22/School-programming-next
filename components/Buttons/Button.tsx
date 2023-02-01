3e5f4b3ae9c3cf8e2ed9f4c6656b1be8147b5e29
PTQ-22 <piotr22.yt@gmail.com>
2023-02-02 00:31:19 +0100

    Fix bugs

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,5 +1,5 @@
-import React, { MouseEventHandler, useState } from 'react'
-import { ButtonFlexWrapper, ButtonsWrapper } from './Button.styles';
+import React, { MouseEventHandler, useState } from "react";
+import { ButtonFlexWrapper, ButtonsWrapper } from "./Button.styles";
 
 interface ButtonsProps {
   code: string;
@@ -7,16 +7,64 @@ interface ButtonsProps {
 }
 
 const CopyIcon = () => {
-  return <svg stroke="#fff" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
-}
+  return (
+    <svg
+      stroke="#fff"
+      fill="none"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+      <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+    </svg>
+  );
+};
 
 const DownloadIcon = () => {
-  return <svg stroke="#fff" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
-}
+  return (
+    <svg
+      stroke="#fff"
+      fill="none"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+      <polyline points="7 10 12 15 17 10"></polyline>
+      <line x1="12" y1="15" x2="12" y2="3"></line>
+    </svg>
+  );
+};
 
 const CheckmarkIcon = () => {
-  return <svg stroke="#fff" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline fill="none" stroke="#fff" stroke-width="2" points="2 14 9 20 22 4"></polyline></svg>
-}
+  return (
+    <svg
+      stroke="#fff"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <polyline
+        fill="none"
+        stroke="#fff"
+        strokeWidth="2"
+        points="2 14 9 20 22 4"
+      ></polyline>
+    </svg>
+  );
+};
 
 export const Buttons = ({ code, downloadName }: ButtonsProps) => {
   const [isCopied, setIsCopied] = useState<boolean>(false);
@@ -31,9 +79,8 @@ export const Buttons = ({ code, downloadName }: ButtonsProps) => {
   };
 
   const handleDownload = (): void => {
-
     const element = document.createElement("a");
-    const file = new Blob([code], {type: 'text/plain'});
+    const file = new Blob([code], { type: "text/plain" });
     element.href = URL.createObjectURL(file);
     element.download = downloadName;
     document.body.appendChild(element);
@@ -48,13 +95,13 @@ export const Buttons = ({ code, downloadName }: ButtonsProps) => {
   return (
     <ButtonsWrapper>
       <ButtonFlexWrapper onClick={handleCopy}>
-        {isCopied ? <CheckmarkIcon /> : <CopyIcon/> }
+        {isCopied ? <CheckmarkIcon /> : <CopyIcon />}
         <span>Skopiuj</span>
       </ButtonFlexWrapper>
       <ButtonFlexWrapper onClick={handleDownload}>
-        {isDownloaded ? <CheckmarkIcon /> : <DownloadIcon/> }
+        {isDownloaded ? <CheckmarkIcon /> : <DownloadIcon />}
         <span>Pobierz</span>
       </ButtonFlexWrapper>
     </ButtonsWrapper>
-  )
-}
+  );
+};
